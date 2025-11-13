d89929ed5dbf27136a3671dd3dbc16b8ecd60028
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2025-11-13 14:46:42 +0100

    extract release.sh

---
+++ scripts/release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -euo pipefail
+
+VERSION="$1"  # first argument passed to the script
+RELEASE_BRANCH="release-$VERSION"
+TAG="v$VERSION"
+
+git config --global user.name "release plugin"
+git config --global user.email "release-plugin@users.noreply.github.com"
+
+# create release branch
+git checkout -b ""$RELEASE_BRANCH""
+
+mvn versions:set -ntp -DnewVersion="$VERSION"
+git add -u
+
+git status
+git config --list --global
+
+git commit -m "Prepare release $VERSION"
+
+git tag -a "$TAG" -m "Release $TAG"
+
+# merge to master
+git checkout master
+git merge --no-edit "$RELEASE_BRANCH"
+
+# merge to develop
+git checkout develop
+git --no-edit "$RELEASE_BRANCH"
+
+# create new snapshot version
+NEXT_VERSION="$(echo "$VERSION" | perl -pe 's{^(([0-9]\.)+)?([0-9]+)$}{$1 . ($3 + 1) . "-SNAPSHOT"}e')"
+mvn versions:set -ntp -DnewVersion="$NEXT_VERSION"
+
+:# alternatively:
+:# mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
+:# NEXT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+git add -u
+git status
+git commit -m "Start next iteration with $NEXT_VERSION"
+
+# push everything
+git push --atomic origin "$RELEASE_BRANCH" master develop --follow-tags # all or nothing
